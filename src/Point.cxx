f45e65c2626012f7c836153ea81f10d312a4813a
ronytigo <120050988+ronytigo@users.noreply.github.com>
2023-03-14 14:23:22 +0200

    Update Point.cxx

--- src/Point.cxx
+++ src/Point.cxx
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2020-2023 offa
+// Copyright (c) 2020-2022 offa
 // Copyright (c) 2019 Adam Wegrzynek
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -29,6 +29,7 @@
 #include "LineProtocol.h"
 #include <chrono>
 #include <memory>
+#include <iostream>
 #include <sstream>
 #include <iomanip>
 
@@ -43,113 +44,125 @@ namespace influxdb
     template <class... Ts>
     overloaded(Ts...) -> overloaded<Ts...>;
 
-    Point::Point(const std::string& measurement)
-        : mMeasurement(measurement), mTimestamp(std::chrono::system_clock::now()), mTags({}), mFields({})
+    // Point::Point(const std::string& measurement)
+    //     : mMeasurement(measurement), mTimestamp(Point::getCurrentTimestamp()), mTags({}), mFields({})
+    // {
+    // }
+    Point::Point(const std::string& line)
+        : mLine(line)
     {
+        std::cout << "mLine is " << mLine << std::endl;
     }
 
-    Point&& Point::addField(std::string_view name, const Point::FieldValue& value)
-    {
-        if (name.empty())
-        {
-            return std::move(*this);
-        }
+    // Point&& Point::addField(std::string_view name, const std::variant<int, long long int, std::string, double>& value)
+    // {
+    //     if (name.empty())
+    //     {
+    //         return std::move(*this);
+    //     }
 
-        mFields.emplace_back(std::make_pair(name, value));
-        return std::move(*this);
+    //     mFields.emplace_back(std::make_pair(name, value));
+    //     return std::move(*this);
+    // }
+
+    // Point&& Point::addTag(std::string_view key, std::string_view value)
+    // {
+    //     if (key.empty() || value.empty())
+    //     {
+    //         return std::move(*this);
+    //     }
+
+    //     mTags.emplace_back(std::make_pair(key, value));
+    //     return std::move(*this);
+    // }
+
+    // Point&& Point::setTimestamp(std::chrono::time_point<std::chrono::system_clock> timestamp)
+    // {
+    //     mTimestamp = timestamp;
+    //     return std::move(*this);
+    // }
+
+    // auto Point::getCurrentTimestamp() -> decltype(std::chrono::system_clock::now())
+    // {
+    //     return std::chrono::system_clock::now();
+    // }
+
+    // std::string Point::toLineProtocol() const
+    // {
+    //     LineProtocol formatter;
+    //     return formatter.format(*this);
+    // }
+
+    // std::string Point::getName() const
+    // {
+    //     return mMeasurement;
+    // }
+
+    // std::chrono::time_point<std::chrono::system_clock> Point::getTimestamp() const
+    // {
+    //     return mTimestamp;
+    // }
+
+    // std::string Point::getFields() const
+    // {
+    //     std::string fields;
+
+    //     for (const auto& field : mFields)
+    //     {
+    //         std::stringstream convert;
+    //         convert << std::setprecision(floatsPrecision);
+
+    //         if (!fields.empty())
+    //         {
+    //             convert << ",";
+    //         }
+
+    //         convert << field.first << "=";
+    //         std::visit(overloaded{
+    //                        [&convert](int v)
+    //                        { convert << v << 'i'; },
+    //                        [&convert](long long int v)
+    //                        { convert << v << 'i'; },
+    //                        [&convert](double v)
+    //                        { convert << std::fixed << v; },
+    //                        [&convert](const std::string& v)
+    //                        { convert << '"' << v << '"'; },
+    //                    },
+    //                    field.second);
+
+    //         fields += convert.str();
+    //     }
+
+    //     return fields;
+    // }
+
+    // std::string Point::getTags() const
+    // {
+    //     if (mTags.empty())
+    //     {
+    //         return "";
+    //     }
+
+    //     std::string tags;
+    //     for (const auto& tag : mTags)
+    //     {
+    //         tags += ",";
+    //         tags += tag.first;
+    //         tags += "=";
+    //         tags += tag.second;
+    //     }
+
+    //     return tags.substr(1, tags.size());
+    // }
+
+    void Point::setLine(std::string line)
+    {
+        this->mLine = line;
     }
 
-    Point&& Point::addTag(std::string_view key, std::string_view value)
+    std::string Point::getLine() const
     {
-        if (key.empty() || value.empty())
-        {
-            return std::move(*this);
-        }
-
-        mTags.emplace_back(std::make_pair(key, value));
-        return std::move(*this);
-    }
-
-    Point&& Point::setTimestamp(std::chrono::time_point<std::chrono::system_clock> timestamp)
-    {
-        mTimestamp = timestamp;
-        return std::move(*this);
-    }
-
-    auto Point::getCurrentTimestamp() -> decltype(std::chrono::system_clock::now())
-    {
-        return std::chrono::system_clock::now();
-    }
-
-    std::string Point::toLineProtocol() const
-    {
-        LineProtocol formatter;
-        return formatter.format(*this);
-    }
-
-    std::string Point::getName() const
-    {
-        return mMeasurement;
-    }
-
-    std::chrono::time_point<std::chrono::system_clock> Point::getTimestamp() const
-    {
-        return mTimestamp;
-    }
-
-    std::string Point::getFields() const
-    {
-        std::stringstream convert;
-        convert << std::setprecision(floatsPrecision) << std::fixed;
-        bool addComma{false};
-        for (const auto& field : mFields)
-        {
-            if (addComma)
-            {
-                convert << ',';
-            }
-
-            convert << field.first << "=";
-            std::visit(overloaded{
-                           [&convert](int v)
-                           { convert << v << 'i'; },
-                           [&convert](long long int v)
-                           { convert << v << 'i'; },
-                           [&convert](double v)
-                           { convert << v; },
-                           [&convert](const std::string& v)
-                           { convert << '"' << v << '"'; },
-                           [&convert](bool v)
-                           { convert << (v ? "true" : "false"); },
-                           [&convert](unsigned int v)
-                           { convert << v << 'u'; },
-                           [&convert](unsigned long long int v)
-                           { convert << v << 'u'; },
-                       },
-                       field.second);
-            addComma = true;
-        }
-
-        return convert.str();
-    }
-
-    std::string Point::getTags() const
-    {
-        if (mTags.empty())
-        {
-            return "";
-        }
-
-        std::string tags;
-        for (const auto& tag : mTags)
-        {
-            tags += ",";
-            tags += tag.first;
-            tags += "=";
-            tags += tag.second;
-        }
-
-        return tags.substr(1, tags.size());
+        return mLine;
     }
 
 } // namespace influxdb
